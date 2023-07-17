62b44e940ecf8723bb594feab958949c512de596
Madhurima Kayal <madhurimakayal97@gmail.com>
2023-07-16 18:26:42 -0700

    removed a previous line and added a new line

--- new_trypy.js
+++ new_trypy.js
@@ -1,5 +1,8 @@
 let findTipPercent = function (total , tipPercent = .2) {
     return total * tipPercent
 }
-let tip = findTipPercent(1000, .30)
+
+// adding a new line and removing the previous print
+
+let tip = findTipPercent(20, .10)
 console.log(tip)
